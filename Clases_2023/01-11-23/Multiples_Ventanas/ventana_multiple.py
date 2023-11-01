4a117aeafab7b77f938d6e4c09fa530d71d363cc
Jorge Gracia <graciajorge.sist@gmail.com>
2023-11-01 19:49:21 -0300

    Update

--- Clases_2023/01-11-23/Multiples_Ventanas/ventana_multiple.py
+++ Clases_2023/01-11-23/Multiples_Ventanas/ventana_multiple.py
@@ -5,17 +5,30 @@ class MiVentana(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("ventana_1.ui", self)
-        self.ventana2 = MiVentana2()
+        # Se pasa el Padre como parámetro al constructor de la Ventana Hijo
+        self.ventana2 = MiVentana2(self)
         self.boton.clicked.connect(self.open_window)
+        self.boton.clicked.connect(self.edit_label_ventana_2)
     
     def open_window(self):
         self.ventana2.show()
 
+    #Comunicación con Ventana Hijo
+    def edit_label_ventana_2(self):
+        self.ventana2.nombre_label.setText(self.nombre.text())
 
 class MiVentana2(QMainWindow):
-    def __init__(self):
+    def __init__(self,padre):
         super().__init__()
         uic.loadUi("ventana_2.ui",self)
+        self.boton_hijo.clicked.connect(self.click_boton)
+        #Se agrega un atributo que guarda todo la Ventana Padre
+        self.padre = padre
+
+    #Comunicación con Ventana Padre
+    def click_boton(self):
+        self.padre.nombre.setText("Hola Mundo!")
+        
     
 app = QApplication([])
 win = MiVentana()
