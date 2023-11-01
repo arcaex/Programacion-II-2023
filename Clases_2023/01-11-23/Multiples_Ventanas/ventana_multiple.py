c9a647f981d7a94a12671918b436ab50b38e385c
Jorge Gracia <graciajorge.sist@gmail.com>
2023-11-01 19:26:52 -0300

    Update Class

--- Clases_2023/01-11-23/Multiples_Ventanas/ventana_multiple.py
+++ Clases_2023/01-11-23/Multiples_Ventanas/ventana_multiple.py
@@ -4,19 +4,22 @@ from PyQt6 import uic
 class MiVentana(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("ventana.ui", self)
+        uic.loadUi("ventana_1.ui", self)
         self.ventana2 = MiVentana2()
-        
+        self.boton.clicked.connect(self.open_window)
+    
+    def open_window(self):
+        self.ventana2.show()
+
 
 class MiVentana2(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("ventana2.ui", self)
+        uic.loadUi("ventana_2.ui",self)
     
 app = QApplication([])
-
 win = MiVentana()
 win.show()
-
-app.exec_()
-
+# win2 = MiVentana2()
+# win2.show()
+app.exec()
