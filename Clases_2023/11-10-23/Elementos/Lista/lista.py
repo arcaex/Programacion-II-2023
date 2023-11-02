357ca029e216b78d595f6bba93884ff30e0a0235
Jorge Gracia <graciajorge.sist@gmail.com>
2023-11-02 19:13:08 -0300

    Update Class

--- Clases_2023/11-10-23/Elementos/Lista/lista.py
+++ Clases_2023/11-10-23/Elementos/Lista/lista.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog
-from PyQt5 import uic
+from PyQt6.QtWidgets import QMainWindow, QApplication, QInputDialog
+from PyQt6 import uic
 
 
 class MiVentana(QMainWindow):
@@ -7,22 +7,35 @@ class MiVentana(QMainWindow):
         super().__init__()
         uic.loadUi("lista.ui", self)
         self.agregar.clicked.connect(self.on_agregar)
-        # self.editar.clicked.connect(self.on_editar)
+        self.editar.clicked.connect(self.on_editar)
         self.lista.itemDoubleClicked.connect(self.on_editar)
         self.quitar.clicked.connect(self.on_quitar)
+        self.filtrar.textChanged.connect(self.filtrare)
+        self.lista_real = []
 
     def on_agregar(self):
+        #.addItem(Texto) - Agregar un Item
         self.lista.addItem(self.nombre.text())
+        self.lista_real.append(self.nombre.text())
         self.nombre.setText('')
-
+    
+    def filtrare(self):
+        self.lista.clear()
+        for item in self.lista_real:
+            if self.filtrar.text() in item:
+                self.lista.addItem(item)
+            
     def on_editar(self):
+        #.currentItem().text() -> Traigo el texto del item seleccionado
+        #.currentItem().setText() -> Cambio el texto del item seleccionado
         texto_item = self.lista.currentItem().text()
-        nuevo_texto, ok = QInputDialog.getText(
-            self, 'Editar', 'Ingrese nuevo nombre', text=texto_item)
+        nuevo_texto, ok = QInputDialog.getText(self, 'Editar', 'Ingrese nuevo nombre', text=texto_item)
         if ok:
             self.lista.currentItem().setText(nuevo_texto)
 
     def on_quitar(self):
+        # .currentRow() -> Fila actual seleccionada
+        # .takeItem(fila) -> Borra una fila
         self.lista.takeItem(self.lista.currentRow())
 
 
@@ -31,4 +44,4 @@ app = QApplication([])
 win = MiVentana()
 win.show()
 
-app.exec_()
+app.exec()
